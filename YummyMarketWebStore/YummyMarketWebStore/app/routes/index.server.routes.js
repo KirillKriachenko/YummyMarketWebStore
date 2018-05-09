c0526ae293e8ce14c50ac53d9feef47adfda51e8
KirillKriachenko <kirillkriachenko@gmail.com>
2018-05-08 19:32:12 -0700

    bsicsv2

---
+++ YummyMarketWebStore/YummyMarketWebStore/app/routes/index.server.routes.js
@@ -0,0 +1,22 @@
+﻿// Load the 'index' controller
+const index = require('../controllers/index.server.controller');
+
+// Define the routes module' method
+module.exports = function (app) {
+    // Mount the 'index' controller's 'render' method
+    app.get('/', index.render);
+    //app.post('/', index.showFeedbackpage);
+    ////redner registration.ejs if a get request is made to /registration path
+    //app.get('/registration', index.renderRegistration);
+    //app.post('/registration', index.registerUser);
+
+    //////renders add_user.ejs if a get request is made to /add_user path
+    //app.get('/feedback', index.renderFeedback);
+    //app.post('/feedback', index.updatFeedback);
+
+    //app.get('/shawallfeedback', index.showallFeedback);
+    //////renders read_user.ejs if a get request is made to /read_user path
+    ////app.get('/read_user', index.renderReadUser);
+
+
+};
